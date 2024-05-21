5631d361733285b301f05de210712064d110d0ba
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2024-05-21 05:42:08 -0700

    Book functions, design and all other responses implemented

--- index.js
+++ index.js
@@ -21,7 +21,62 @@ app.use(
 app.use(express.static("public"))
 
 app.get("/", async (req,res) =>{
-    res.render("index.ejs")
+    try {
+        res.render("index.ejs")
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+    }
+})
+
+app.post(`/`, async (req,res) =>{
+    try {
+        const { bookTitle, bookDescription, dateRead, scale } = req.body;
+        const value = req.body.isbn
+    
+        const response = await axios.get(`https://covers.openlibrary.org/b/isbn/${value}-M.jpg`);
+        const result = response.data;
+
+        // await db.query(
+        //     `
+        //     INSERT INTO books (title, description, date_read, scale, isbn, cover_url)
+        //     VALUES ($1, $2, $3, $4, $5, $6)
+        //     RETURNING *;
+        //     `, [bookTitle, bookDescription, dateRead,scale,value,coverUrl]
+        // )
+
+        // const newBook = db.rows[0];
+
+    //     const insertQuery = `
+    //         INSERT INTO books (title, description, date_read, scale, isbn, cover_url)
+    //         VALUES ($1, $2, $3, $4, $5, $6)
+    //         RETURNING *;`;
+
+    //   const values = [bookTitle, bookDescription, dateRead, scale, isbn, coverUrl];
+  
+    //   const dbResponse = await db.query(insertQuery, values);
+    //   const newBook = dbResponse.rows[0];
+
+
+        res.render("index.ejs",{
+            data: {
+                bookTitle: bookTitle,
+                bookDescription: bookDescription,
+                dateRead: dateRead,
+                scale: scale,
+                coverUrl: result
+            }
+
+            // data: newBook
+        })
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+          error: error.message,
+        });
+    }
 })
 
 app.listen(port, () => {
